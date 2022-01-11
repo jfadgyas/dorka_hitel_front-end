febbe4e34ba77cc2f13c35f6ac0e29372314a076
jfadgyas <djdeville72@yahoo.com>
2022-01-11 17:06:53 +0100

    scroll

--- src/Components/Calculator/CalcResDetails.js
+++ src/Components/Calculator/CalcResDetails.js
@@ -77,7 +77,7 @@ const CalcResDetails = props => {
             </ul>
             {detailData.length !== 0 && <div className='res-detail__currentBox'
                 onTouchStart={handleTouch}
-                onTouchMove={e => e.preventDefault()}
+                onTouchMove={e.preventDefault()}
                 onTouchEnd={handleTouch}
                 onMouseDown={handleMouse}
                 onMouseUp={handleMouse}></div>
